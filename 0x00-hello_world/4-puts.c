885985d3373d3174380a5daab7eb71197fc6f7f0
yonas1291 <yonasbehayilu79@gmail.com>
2022-12-08 13:41:10 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stduio.h>
+#include <stdio.h>
 /**
- * main - main block
+ * main -main block
  * return: 0
  */
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual puzzle");
-	return (0); 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
