aa2426d0278679bf4260819fc9871916b8446d94
yonas1291 <yonasbehayilu79@gmail.com>
2022-12-09 14:49:34 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**                                                           
  * main -main block     
- * Description: Get a random number and check its last digtit, compre it with 5
+ * Description: Get a random number and check its last digtit, compare it with 5
  * Return: 0
  */
 int main(void)                                              
