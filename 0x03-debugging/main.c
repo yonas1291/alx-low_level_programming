89c9a9e4976548b7b99c0ea70cfa622cb95669c3
yonas1291 <yonasbehayilu79@gmail.com>
2022-12-15 12:56:24 -0800

    main.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -4,29 +4,19 @@
 
 /**
  *
- *  * main - tests function that prints if integer is positive or negative
- *
- *   * Return: 0
- *
- *    */
-
-
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 
 {
+ int i;
 
-	 int i;
+ i = 98;
+ positive_or_negative(i);
 
-
-
-	  i = 98;
-
-	   positive_or_negative(i);
-
-
-
-	    return (0);
+return (0);
 
 }
 
