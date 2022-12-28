7e462cdbb61130d92c43295720cccf95e868d7b1
yonas1291 <yonasbehayilu79@gmail.com>
2022-12-28 10:31:32 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * description: get a random number and check its
+ * return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q'i)
+		{
+		
+	         	putchar(C);
+		
+		}c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
